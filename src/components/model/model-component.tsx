9f334b5f34a5f1a9064ced733623160e8c606c4a
eireland <eireland@concord.org>
2023-12-18 16:16:36 -0800

    Fixes slider thumbnail sliding

--- src/components/model/model-component.tsx
+++ src/components/model/model-component.tsx
@@ -4,6 +4,30 @@ import { IDevice } from "../../models/device-model";
 
 import "./model-component.scss";
 
+const SpeedSlider = () => {
+  const [value, setValue] = useState(1);
+  const speedValue = ["Slow", "Medium", "Fast", "Fastest"];
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(parseInt(event.target.value, 10)); // Parse the value as an integer
+  };
+
+  return (
+    <div className="speed-slider">
+      <input
+        type="range"
+        min={0}
+        max={3}
+        value={value}
+        onChange={handleChange}
+        step={1}
+      />
+      <span id="speed-text" data-text="DG.plugin.Sampler.top-bar.medium-speed">
+        {speedValue[value]}
+      </span>
+    </div>
+  );
+};
+
 export const ModelTab = () => {
   const [numDevices, setNumDevices] = useState(1);
   const [selectedDevice, setSelectedDevice] = useState<IDevice | null>(null);
@@ -13,18 +37,7 @@ export const ModelTab = () => {
       <div className="model-controls">
         <button className="start-button">START</button>
         <button className="stop-button">STOP</button>
-        <div className="speed-slider">
-          <input id="speed" type="range" min="0" max="3" value="1" list="speedsettings" />
-          <datalist id="speedsettings">
-            <option>0</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-          </datalist>
-          <span id="speed-text" data-text="DG.plugin.Sampler.top-bar.medium-speed">
-            Medium
-          </span>
-        </div>
+        <SpeedSlider />
         <button className="clear-data-button">CLEAR DATA</button>
       </div>
       <div className="select-repeat-controls">
