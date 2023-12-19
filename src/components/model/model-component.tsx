7268937dd3eeb02c655499d9627503b969da3b04
eireland <eireland@concord.org>
2023-12-18 20:12:18 -0800

    Adds repeat until help modal
    Adds repeat until condition input

--- src/components/model/model-component.tsx
+++ src/components/model/model-component.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Device } from "./device";
 import { IDevice } from "../../models/device-model";
+import InfoIcon from "../../assets/help-icon.svg";
 
 import "./model-component.scss";
 
@@ -42,6 +43,16 @@ const SpeedSlider = () => {
 export const ModelTab = () => {
   const [numDevices, setNumDevices] = useState(1);
   const [selectedDevice, setSelectedDevice] = useState<IDevice | null>(null);
+  const [repeat, setRepeat] = useState(false);
+  const [showHelp, setShowHelp] = useState(false);
+
+  const handleSelectRepeat = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setRepeat(e.target.value === "repeat");
+  };
+
+  const handleOpenHelp = () => {
+    setShowHelp(!showHelp);
+  };
 
   return (
     <div className="model-tab">
@@ -52,21 +63,30 @@ export const ModelTab = () => {
         <button className="clear-data-button">CLEAR DATA</button>
       </div>
       <div className="select-repeat-controls">
-        <div className="select-repeat-dropdown">
-          <select>
-            <option className={`select-repeat-option`} value="select">Select</option>
-            <option className={`select-repeat-option`} value="repeat">Repeat</option>
-          </select>
+        <div className="select-repeat-selection">
+          <div className="select-repeat-dropdown">
+            <select onChange={handleSelectRepeat}>
+              <option className={`select-repeat-option`} value="select">Select</option>
+              <option className={`select-repeat-option`} value="repeat">Repeat</option>
+            </select>
+          </div>
+          <input id="sample_size" defaultValue="5"></input>
+          <span>items</span>
+          <div className="select-replacement-dropdown">
+            <select>
+              <option>with replacement</option>
+              <option>without replacement</option>
+            </select>
+          </div>
         </div>
-        <input id="sample_size" defaultValue="5"></input>
-        <span>items</span>
-        <div className="select-replacement-dropdown">
-          <select>
-            <option>with replacement</option>
-            <option>without replacement</option>
-          </select>
-        </div>
-
+        {repeat &&
+          <div className="repeat-until-controls">
+            <span>until</span>
+            <input type="text"></input>
+            <InfoIcon onClick={handleOpenHelp}/>
+            {showHelp && <HelpModal setShowHelp={setShowHelp}/>}
+          </div>
+        }
       </div>
       <div className="collect-controls">
         <span>Collect</span>
@@ -84,6 +104,46 @@ export const ModelTab = () => {
             <div></div>
           </div>
         </div>
+      </div>
+    </div>
+  );
+};
+
+interface IHelpModal {
+  setShowHelp: (show: boolean) => void;
+}
+
+const HelpModal = ({setShowHelp}: IHelpModal) => {
+  const handleCloseModal = () => {
+    setShowHelp(false);
+  };
+  return (
+    <div className="help-modal">
+      <div className="modal-header">
+        Help: Repeat Until Condition
+      </div>
+      <div className="modal-body">
+        <p className="help-section-title">
+          Specify a Condition to Repeat Until
+        </p>
+        <p className="help-section-body">
+          Repeat Until allos the odel to continue drawing samples until a desired outcome -- the Condition specified -- occurs.
+        </p>
+        <p className="help-section-title">
+          Using a Formula
+        </p>
+        <p className="help-section-body">
+          {`Example: sex = "male" AND height > 5`}
+        </p>
+        <p className="help-section-title">
+          Using a Pattern
+        </p>
+        <p className="help-section-body">
+          {`Example: a,b,a`}
+        </p>
+      </div>
+      <div className="modal-footer">
+        <button className="modal-button" onClick={handleCloseModal}>Close</button>
       </div>
     </div>
   );
