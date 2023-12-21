2195388d76788772712e5c955aa3779b0a600a09
lublagg <lvblagg@gmail.com>
2023-12-21 18:22:02 -0500

    Add documentation to device model.

--- src/models/device-model.tsx
+++ src/models/device-model.tsx
@@ -7,18 +7,22 @@ export interface IDevice {
   variables: IVariables | ICollectorVariables;
 }
 
-// mixer / spinner
-// input: IVariables
-// output: {[nameOfAttr]: randomly-selected variable}
+// a map of variables to their percentages
+// i.e.: { a: 50, b: 50 }
+// the view (mixer / spinner) decides how to best represent these percentages
 export interface IVariables {
   [variableName: string]: number;
 }
 
-// collector
-// input: ICollectorVariables
-// output: randomly-selected ICollectorVariable
-export interface ICollectorVariable {
+// a map of attributes to their values
+// { "Mammal": "Dog", "Color": "Brown"}
+export interface ICollectorItem {
   [attr: string]: string | number;
 }
 
-export type ICollectorVariables = Array<ICollectorVariable>;
+// an array of items that come from a linked dataset
+// i.e.: [ { "Mammal": "Dog", "Color": "Brown"}, { "Mammal": "Cat", "Color": "Black"} ]
+// the collector view will represent the values of the first key-value pair of each object in the array
+// i.e., one ball for "Dog", one ball for "Cat"
+// if "Dog" is selected by the collector, the entire item is sent to CODAP
+export type ICollectorVariables = Array<ICollectorItem>;
