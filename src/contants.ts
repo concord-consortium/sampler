2c85d8ca0676879bf6822ae6c1ae5d4098f703df
Doug Martin <dmartin@concord.org>
2024-10-02 05:57:14 -0400

    build: Update version to 0.3.0

--- src/contants.ts
+++ src/contants.ts
@@ -1,7 +1,7 @@
 export const kDataContextName = "Sampler";
 export const kPluginMidWidth = 328;
 export const kPluginName = "Sampler";
-export const kVersion = "v0.2.0";
+export const kVersion = "v0.3.0";
 export const kInitialDimensions = {
   width: kPluginMidWidth,
   height: 500
