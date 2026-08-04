13bf7d0137172cad70344d64c4d947216ed87b09
Kirk Swenson <kswenson@users.noreply.github.com>
2026-08-04 14:20:33 -0700

    build: Update version to 1.0.5

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import { tr } from "./utils/localeManager";
 
 export const kPluginMidWidth = 328;
 export const kPluginName = tr("DG.Plugin.Sampler.title");
-export const kVersion = "v1.0.4";
+export const kVersion = "v1.0.5";
 export const kInitialDimensions = {
   width: kPluginMidWidth,
   height: 550
